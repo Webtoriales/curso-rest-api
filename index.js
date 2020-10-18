3a32c4868796a8d807e81113e1c279d64e341617
Andreas <andreaslorozco@gmail.com>
2020-10-17 18:40:51 -0600

    adds lesson 02

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/athletes', (req, res) => {
+  res.send('Obteniendo el recurso de atletas');
+});
+
+app.get('/api/sports', (req, res) => {
+  res.send('Obteniendo el recurso de deportes.');
+});
+
 app.listen(8080, () => {
   console.log('App listening on port 8080');
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # Webtoriales
 
 ## Curso Express y MongoDB
+
+REST Representational State Transfer
+Transferencia de Estado Representacional
+
+* Cliente - Servidor
+
+URLs Basadas en Recursos.
+
+www.uefa.com/equipos 
+
+/api/atletas
+/api/deportes
