6074e34d6055806ef876907d4e41ac44e6d152ac
Andreas <andreaslorozco@gmail.com>
2020-10-25 15:14:53 -0600

    adds lesson 04

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ app.get('/api/athletes', (req, res) => {
   res.send('Obteniendo el recurso de atletas!!');
 });
 
+app.post('/api/athletes', (req, res) => {
+  res.send('Creando un nuevo recurso de atletas.');
+});
+
 app.get('/api/sports', (req, res) => {
   res.send('Obteniendo el recurso de deportes.');
+});
+
+app.post('/api/sports', (req, res) => {
+  res.send('Creando un nuevo recurso de deportes.');
 });
 
 app.listen(8080, () => {
