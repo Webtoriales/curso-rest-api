d05940d7a1689a06c88c9dbd242cf29795624727
Andreas <andreaslorozco@gmail.com>
2020-10-25 14:51:38 -0600

    adds lesson 03

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/athletes', (req, res) => {
-  res.send('Obteniendo el recurso de atletas');
+  res.send('Obteniendo el recurso de atletas!!');
 });
 
 app.get('/api/sports', (req, res) => {
