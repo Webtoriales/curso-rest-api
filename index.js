9f98666aa2e678dae03c254fd70531628c2a801e
Andreas <andreaslorozco@gmail.com>
2020-10-28 16:19:27 -0600

    adds lesson 5

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const middlewares = require('./middlewares');
+
 const app = express();
 
 app.get('/api/athletes', (req, res) => {
@@ -16,6 +19,9 @@ app.get('/api/sports', (req, res) => {
 app.post('/api/sports', (req, res) => {
   res.send('Creando un nuevo recurso de deportes.');
 });
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 app.listen(8080, () => {
   console.log('App listening on port 8080');
--- readme.md
+++ readme.md
@@ -1,15 +1,6 @@
-# Webtoriales
+# Middlewares
 
-## Curso Express y MongoDB
-
-REST Representational State Transfer
-Transferencia de Estado Representacional
-
-* Cliente - Servidor
-
-URLs Basadas en Recursos.
-
-www.uefa.com/equipos 
-
-/api/atletas
-/api/deportes
+* Request del Cliente - Middleware - Respuesta del Servidor
+* Ciclo Solicitud - Respuesta
+* Acceso los objetos REQ y RES
+* Encandables
